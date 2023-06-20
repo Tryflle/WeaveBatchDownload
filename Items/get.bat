9d883c1288ec483a0d22ee5eabe1374aacfaca81
Tryfle <111710533+Tryflle@users.noreply.github.com>
2023-06-19 20:54:45 -0400

    Update LCQT link

--- Items/get.bat
+++ Items/get.bat
@@ -1,4 +1,4 @@
-powershell -Command "Invoke-WebRequest https://github.com/Youded-byte/lunar-client-qt/releases/download/v1.5.2/windows.zip -OutFile LCQT.zip"
+powershell -Command "Invoke-WebRequest https://github.com/Youded-byte/lunar-client-qt/releases/download/v1.5.3/windows.zip -OutFile LCQT.zip"
 powershell -Command "Invoke-WebRequest https://github.com/Weave-MC/Weave-Loader/releases/download/v0.2.3/Weave-Loader-Agent-0.2.3.jar -OutFile Weave-Loader-Agent-0.2.3.jar"
 tar -xf LCQT.zip
 del LCQT.zip
