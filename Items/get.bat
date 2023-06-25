0fd36bc90260fcc58bd777af8319aa5b687a57e7
Tryfle <111710533+Tryflle@users.noreply.github.com>
2023-06-24 20:12:50 -0400

    added @echo off.. about time

--- Items/get.bat
+++ Items/get.bat
@@ -1,3 +1,4 @@
+@echo off
 powershell -Command "Invoke-WebRequest https://github.com/Youded-byte/lunar-client-qt/releases/download/v1.5.3/windows.zip -OutFile LCQT.zip"
 powershell -Command "Invoke-WebRequest https://github.com/Weave-MC/Weave-Loader/releases/download/v0.2.3/Weave-Loader-Agent-0.2.3.jar -OutFile Weave-Loader-Agent-0.2.3.jar"
 tar -xf LCQT.zip
