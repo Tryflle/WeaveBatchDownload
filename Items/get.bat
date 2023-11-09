868d71f862d0f2267fef165dd663f35c7d9fb124
Tryfle <111710533+Tryflle@users.noreply.github.com>
2023-11-09 17:44:48 -0500

    Update get.bat

--- Items/get.bat
+++ Items/get.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell -Command "Invoke-WebRequest https://github.com/Youded-byte/lunar-client-qt/releases/download/v1.5.3/windows.zip -OutFile LCQT.zip"
+powershell -Command "Invoke-WebRequest https://github.com/Youded-byte/lunar-client-qt/releases/download/v1.5.4/windows.zip -OutFile LCQT.zip"
 powershell -Command "Invoke-WebRequest https://github.com/Weave-MC/Weave-Loader/releases/download/v0.2.4/Weave-Loader-Agent-0.2.4.jar -OutFile Weave-Loader-Agent-0.2.4.jar"
 tar -xf LCQT.zip
 del LCQT.zip
